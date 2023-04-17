87a69d5986b10f7184fb03dca6ae956c300a784a
Lucas <lucaskeller98@gmail.com>
2023-04-17 23:13:02 +0200

    enhance colors of embed

--- commands/draw.js
+++ commands/draw.js
@@ -32,7 +32,7 @@ module.exports = {
       type: card.type,
       price: card.card_prices[0].cardmarket_price,
       img: card.card_images[0].image_url,
-      rarity: card.card_sets[Math.floor(Math.random() * card.card_sets.length)].set_rarity,
+      rarity: card.card_sets[Math.floor(Math.random() * card.card_sets?.length)].set_rarity,
     };
 
     const accept = new ButtonBuilder()
@@ -50,7 +50,7 @@ module.exports = {
     const actionRow = new ActionRowBuilder().addComponents(accept, denial);
 
     const embed = new EmbedBuilder()
-      .setColor(0x95f99f)
+      .setColor(getColorForCardType(cardInfo.type))
       .setTitle(cardInfo.name)
       .setImage(cardInfo.img)
       .setThumbnail(interaction.user.displayAvatarURL())
@@ -94,14 +94,15 @@ module.exports = {
             });
           }
         }
-
-        await i.update({ components: [] });
+        embed.setColor(0x00ff00); // Green color
+        await i.update({ embeds: [embed], components: [] });
         await i.followUp({
           content: "The card was sleeved and carefully put in your Binder!",
           ephemeral: true,
         });
       } else if (i.customId === "denial_button_id") {
-        await i.update({ components: [] });
+        embed.setColor(0xff0000); // Red color
+        await i.update({ embeds: [embed], components: [] });
         await i.followUp({
           content: "The Card was burned and floats now in the shadow realm!",
           ephemeral: true,
@@ -130,3 +131,23 @@ module.exports = {
     }, 900000);
   },
 };
+
+function getColorForCardType(type) {
+  const typeColors = {
+    Normal: 0xffff00, // Yellow
+    Effect: 0xffa500, // Orange
+    Ritual: 0xadd8e6, // Light blue
+    Fusion: 0xee82ee, // Violet
+    Synchro: 0xffffff, // White
+    Xyz: 0x000000, // Black
+    Pendulum: 0x008000, // Green
+    Link: 0x00008b, // Dark blue
+    Spell: 0x008000, // Green
+    Trap: 0x800080, // Purple
+    Token: 0x808080, // Gray
+  };
+
+  // Find the first color that matches a partial key in the typeColors object
+  const color = Object.keys(typeColors).find((key) => type.includes(key));
+  return typeColors[color] || 0xffff00; // Default color if type not found
+}
