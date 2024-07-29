61af5c2de20bb034db0bb820e0b103c1f2878549
Lucas <lucaskeller98@gmail.com>
2024-07-29 10:22:51 +0200

    migrate dbFunctions to typescript 🩵

--- db/dbFunctions.ts
+++ db/dbFunctions.ts
@@ -1,18 +1,24 @@
 import fs from 'fs';
+import { Binder } from '../shared/models/binder.models.js';
 
-function readDb(dbName = 'db.json') {
-	// read JSON object from file
-	const data = fs.readFileSync(dbName, 'utf8');
-	return JSON.parse(data);
+function readDb(dbName: string = 'db.json'): Binder[] {
+	try {
+		// read JSON object from file
+		const data = fs.readFileSync(dbName, 'utf8');
+		return JSON.parse(data) as Binder[];
+	} catch (error) {
+		console.error('Error reading database:', error);
+		return [];
+	}
 }
 
-function getUserData(userId, dbName = 'db.json') {
+function getUserData(userId: string, dbName: string = 'db.json'): Binder {
 	const data = readDb(dbName);
 
 	const dateString = new Date().toLocaleDateString();
-	const userData = data.find((item) => item.userId == userId);
+	const userData = data.find((item) => item.userId === userId);
 	if (!userData) {
-		const initialData = {
+		const initialData: Binder = {
 			userId: userId,
 			currency: 0,
 			dailyPurchases: {
@@ -25,8 +31,8 @@ function getUserData(userId, dbName = 'db.json') {
 		return initialData;
 	}
 
-	if (!userData?.dailyPurchases || userData?.dailyPurchases.date !== dateString) {
-		userData['dailyPurchases'] = {
+	if (!userData.dailyPurchases || userData.dailyPurchases.date !== dateString) {
+		userData.dailyPurchases = {
 			date: dateString,
 			packs: {},
 		};
@@ -35,10 +41,13 @@ function getUserData(userId, dbName = 'db.json') {
 	return userData;
 }
 
-function writeDb(obj, dbName = 'db.json') {
-	if (!obj) return console.log('Please provide data to save');
+function writeDb(obj: Binder, dbName: string = 'db.json'): void {
+	if (!obj) {
+		console.log('Please provide data to save');
+		return;
+	}
 	try {
-		const data = JSON.parse(fs.readFileSync(dbName, 'utf8'));
+		const data = readDb(dbName);
 		const index = data.findIndex((item) => item.userId === obj.userId);
 		if (index !== -1) {
 			data[index] = obj;
@@ -46,9 +55,8 @@ function writeDb(obj, dbName = 'db.json') {
 			data.push(obj);
 		}
 		fs.writeFileSync(dbName, JSON.stringify(data)); // overwrites current data
-		return;
 	} catch (err) {
-		return console.log('FAILED TO WRITE');
+		console.error('FAILED TO WRITE:', err);
 	}
 }
 
